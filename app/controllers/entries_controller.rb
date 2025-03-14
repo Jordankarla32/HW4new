21539ca45bac82e2d994bae570c5d8a14f29ecae
Jordan Karla <jordan.karla@kellogg.northwestern.edu>
2025-03-14 04:01:49 +0000

    photos displaying. need to still fix showing everyones entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,20 +8,24 @@ class EntriesController < ApplicationController
 
   def create
     @entry = Entry.new(entry_params)
-    @entry.user_id = session[:user_id]  # Assign to logged-in user
-
+    @entry.user_id = session[:user_id]  # Ensure logged-in user is assigned
+    @entry.place_id = params[:place_id] if params[:place_id]  # Assign place_id if missing
+  
     if @entry.save
       redirect_to "/places/#{@entry.place_id}", notice: "Entry created successfully!"
     else
+      flash[:alert] = "Failed to create entry: #{@entry.errors.full_messages.join(', ')}"
       render :new, status: :unprocessable_entity
     end
   end
+  
 
   private
 
   def entry_params
-    params.require(:entry).permit(:title, :description, :occurred_on, :place_id, :image)
+    params.require(:entry).permit(:title, :description, :occurred_on, :place_id, :image, :user_id)
   end
+  
 
   def require_login
     unless session[:user_id]
